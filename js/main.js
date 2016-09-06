10dd5d09b0fbd57775f956a14af2ba0b56010a66
wanghuanpc <wanghuanpc>
2016-09-06 15:48:08 +0800

    2016-9-6 d

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(function(){
     buy_hover();
     list_imgs();
+    buy_click();
 });
 
 function buy_hover() {
@@ -10,6 +11,18 @@ function buy_hover() {
             $(this).removeClass("tada");
         }
     );
+}
+
+function buy_click(){
+    $(".buy_a").click(function (e) {
+        var explorer =navigator.userAgent;
+        if(!explorer.match(/MicroMessenger/i)){  //不是微信
+            return true;
+        }else{ //是微信
+            $("#wx").show();
+            return false;
+        }
+    });
 }
 
 function list_imgs() {
